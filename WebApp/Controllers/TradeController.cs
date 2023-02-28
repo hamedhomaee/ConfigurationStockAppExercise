7891e5907b82c174c2d3a6bb47a30c0fb47d3560
hamedhomaee <hamed.homaee@waits-gmbh.de>
2023-02-28 13:45:11 +0100

    style added, some important modifications added

--- WebApp/Controllers/TradeController.cs
+++ WebApp/Controllers/TradeController.cs
@@ -1,4 +1,6 @@
 using ConfigurationStocksApp.Options;
+using ConfigurationStocksApp.ServiceContracts;
+using ConfigurationStocksApp.WebApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 
@@ -7,17 +9,31 @@ namespace ConfigurationStocksApp.WebApp.Controllers;
 public class TradeController : Controller
 {
     private readonly TradingOptions? _options;
+    private readonly IFinnhubService _finnhubService;
 
-    public TradeController(IOptions<TradingOptions> options)
+    public TradeController(IOptions<TradingOptions> options, IFinnhubService finnhubService)
     {
+        _finnhubService = finnhubService;
         _options = options.Value;
     }
 
     [Route("/")]
-    public IActionResult Index()
+    public async Task<IActionResult> IndexAsync()
     {
-        
         ViewBag.Title = "Welcome to Stock Trade";
-        return View();
+
+        Dictionary<string, Object>? priceQuote = await _finnhubService!.GetStockPriceQuoteAsync(_options!.DefaultStockSymbol!);
+
+        Dictionary<string, Object>? companyProfile = await _finnhubService!.GetCompanyProfileAsync(_options!.DefaultStockSymbol!);
+
+        StockTrade model = new()
+        {
+            StockSymbol = _options.DefaultStockSymbol,
+            StockName = Convert.ToString(companyProfile!["name"]),
+            Price = Convert.ToDouble(priceQuote!["c"]),
+            Quantity = 100
+        };
+
+        return View(model);
     }
 }
--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,10 +1,20 @@
+using ConfigurationStocksApp.FinnhubService;
 using ConfigurationStocksApp.Options;
+using ConfigurationStocksApp.ServiceContracts;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
 builder.Services.Configure<TradingOptions>(builder.Configuration.GetSection("TradingOptions"));
+
+builder.Services.AddScoped<IFinnhubService>(provider =>
+{
+    var myOptions = builder.Services.BuildServiceProvider().GetRequiredService<IOptions<TradingOptions>>().Value;
+
+    return new FinnhubService(myOptions.FinnhubAPIKey!);
+});
 
 var app = builder.Build();
 
